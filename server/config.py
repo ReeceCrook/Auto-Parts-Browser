f5abfb32393c534051684e3e42055f21be7f3386
Reece <reececrook16@gmail.com>
2025-08-18 17:52:01 -0600

    Fixed CELERY_BROKER_URL and CELERY_RESULT_BACKEND still being the old localhost rather than the new render background worker

--- server/config.py
+++ server/config.py
@@ -8,8 +8,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_COOKIE_SAMESITE = 'Lax'
     JSON_COMPACT = False
-    CELERY_BROKER_URL = 'redis://localhost:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+    CELERY_BROKER_URL = 'redis://red-d2hmkgv5r7bs738c36ng:6379'
+    CELERY_RESULT_BACKEND = 'redis://red-d2hmkgv5r7bs738c36ng:6379'
     CELERY_IMPORTS = ("server.tasks.advance_scraper", "server.tasks.oreilly_scraper", "server.tasks.get_places",)
     broker_connection_retry_on_startup = True
     CELERY_TRACK_STARTED=True

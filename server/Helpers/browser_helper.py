380a6bd5dcd43c9a6d54fcddd266f3ae48f32280
Reece <reececrook16@gmail.com>
2025-09-02 17:22:17 -0600

    Switched out playwright 'disable-features=HTTP2' for 'disable-http2' to try to avoid render getting the HTTP2 error || Also added an 'add_init_scrip' line setting the navigator.webdriver to undefined to help avoid bot detection

--- server/Helpers/browser_helper.py
+++ server/Helpers/browser_helper.py
@@ -8,13 +8,17 @@ async def launch_browser(user_agent=None, viewport=None):
         "--disable-quic",
         "--disable-blink-features=AutomationControlled",
         '--disable-infobars',
-        '--disable-features=HTTP2'
+        "--disable-http2",
     ])
 
     context = await browser.new_context(
         user_agent=user_agent or "default-user-agent",
         viewport=viewport,
         bypass_csp=True
+    )
+
+    await context.add_init_script(
+        "Object.defineProperty(navigator, 'webdriver', { get: () => undefined });"
     )
 
     return p, browser, context
